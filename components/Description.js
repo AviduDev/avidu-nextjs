0a2a00af2f87e275b8bd6250e059a598b556a2d5
AviduDev <avidudev@gmail.com>
2023-03-17 16:57:42 +0530

    des added

--- components/Description.js
+++ components/Description.js
@@ -5,9 +5,9 @@ export default function Description() {
     <section className={styles.section}>
       <div className={styles.dscContainer}>
         <h1>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Error,
-          sapiente architecto non voluptatum nam illum dolorum libero
-          repellendus distinctio inventore maxime omnis magnam quod iure!
+          Avidu is a creative web developer and designer from sri lanka who
+          builds modern, fast, and const-effective websites using modern tech
+          stacks
         </h1>
       </div>
     </section>
