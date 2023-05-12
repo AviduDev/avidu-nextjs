e348662edc9b53cdeeb71f006dc36aa87427670a
Avidu <109849172+AviduDev@users.noreply.github.com>
2023-05-12 12:12:25 +0530

    Update _app.js
    Lenis easing changed

--- pages/_app.js
+++ pages/_app.js
@@ -5,11 +5,11 @@ export default function App({ Component, pageProps }) {
   return (
     <ReactLenis root options={{ 
       duration: 2,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
+      easing: (t) => Math.min(1, 1.001 - Math.pow(1, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
       direction: "vertical", // vertical, horizontal
       gestureDirection: "vertical", // vertical, horizontal, both
       smooth: true,
-      mouseMultiplier: 5,
+      mouseMultiplier: 2,
       smoothTouch: false,
       touchMultiplier: 0.2,
       infinite: false
